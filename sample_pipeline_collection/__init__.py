50316943f6d98d135ed00919b3bf345ab4a3da22
Camille Avestruz <camille.avestruz@yale.edu>
2018-10-23 07:52:54 -0500

    example pipeline collection initiated

---
+++ sample_pipeline_collection/__init__.py
@@ -0,0 +1,4 @@
+# Make sure any stages you want to use in a pipeline
+# are imported here.
+from ceci import PipelineStage
+from .sample_pipeline_collectionStage1 import sample_pipeline_collectionStage1
